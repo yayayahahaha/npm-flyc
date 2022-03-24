d66137a1df28e49ab145148ea1ac456a624091e3
flyc <yayayahahahaooii@gmail.com>
2022-03-24 12:47:16 +0800

    refactored donwload.js

--- utils/download.js
+++ utils/download.js
@@ -1,50 +1,48 @@
-var fs = require('fs');
-var axios = require('axios');
+const fs = require('fs')
+const axios = require('axios')
 
-function download(url, filePath, {
-    headers = {},
-    callback = Function.prototype,
-    callbackParameter = undefined
-} = {}) {
-    return new Promise(async (resolve, reject) => {
-        // 濾掉尾巴的斜線
-        if (/\/$/.test(filePath)) {
-            filePath = filePath.slice(0, filePath.length - 1);
-        }
-        // 濾掉開頭的./
-        if (/^\.\//.test(filePath)) {
-            filePath = filePath.slice(2, filePath.length);
-        }
+// TODO 避免單純使用 headers, 要傳入整個 axios condif 做 assign 之類的
 
-        // 如果資料夾不存在會自動創建的系統
-        var paths = filePath.split('/'),
-            createdDirectory = [];
-        for (var i = 0; i < paths.length - 1; i++) {
-            createdDirectory.push(paths[i]);
-            var checkedDirectory = createdDirectory.join('/');
-            !fs.existsSync(checkedDirectory) && fs.mkdirSync(checkedDirectory);
-        }
+function download(
+  url,
+  rowFilePath,
+  { headers = {}, callback = Function.prototype, callbackParameter = undefined } = {}
+) {
+  return new Promise((resolve, reject) => {
+    // 濾掉尾巴的斜線和開頭的./
+    const filePath = rowFilePath.replace(/^\./, '').replace(/\/$/, '')
 
-        var file = fs.createWriteStream(filePath);
-        axios({
-            method: 'get',
-            url: url,
-            responseType: 'stream',
-            headers: headers
-        }).then(({
-            data
-        }) => {
-            callback(true, callbackParameter);
-            data.pipe(file);
-            file.on('finish', () => {
-                resolve(true);
-            });
-        }).catch((error) => {
-            console.log(error);
-            callback(false, callbackParameter);
-            reject([null, error]);
-        });
-    });
+    // 如果資料夾不存在會自動創建的系統
+    const paths = filePath.split('/')
+    const createdDirectory = []
+
+    for (let i = 0; i < paths.length - 1; i++) {
+      createdDirectory.push(paths[i])
+      const checkedDirectory = createdDirectory.join('/')
+
+      !fs.existsSync(checkedDirectory) && fs.mkdirSync(checkedDirectory)
+    }
+
+    const file = fs.createWriteStream(filePath)
+    const axiosSetting = {
+      method: 'get',
+      url,
+      responseType: 'stream',
+      headers
+    }
+    axios(axiosSetting)
+      .then(({ data /* 這個是 axios 的 data */ }) => {
+        callback(true, callbackParameter)
+        data.pipe(file)
+
+        file.on('finish', () => resolve(true))
+      })
+      .catch(error => {
+        console.log(error)
+        callback(false, callbackParameter)
+        reject([null, error])
+      })
+  })
 }
 
-module.exports = download;
+module.exports = download
