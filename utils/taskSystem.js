c0843cee458f43562790d5e16f5e95a749946fad
flyc <yayayahahahaooii@gmail.com>
2023-12-29 20:52:12 +0800

    use cli-progress bar

--- utils/taskSystem.js
+++ utils/taskSystem.js
@@ -1,9 +1,12 @@
 // TODO:
+// 0. 整理 TODO
 // 1. 當前的這個taskSystem 執行的時候是mute 的還是verbose 的
-// 2. refect 的時候的 retry? 像是把 reject 的項目放到task 的後面之類的, 然後還要計次
+// 2. reject 的時候的 retry? 像是把 reject 的項目放到task 的後面之類的, 然後還要計次
 // 3. 區分警告訊息與正規訊息
 // 4. log: 記錄中間失敗的過程、產生一個 hash 記錄每次的執行狀況等
 // 5. 調整檢查參數那邊的 code 變得更優雅一些
+// 6. 用套件處理 progress 的部分, 後續再透過參數決定要不要顯示
+// 7. 每一個 job 如果可以有自己的 callback 的話, 看可不可以顯示巢狀的任務進度表? (e.g. download progress)
 
 /*
 需求
@@ -35,6 +38,8 @@
   }
 }
 */
+const cliProgress = require('cli-progress')
+
 const isPositiveInt = (number) => /^[1-9]\d*$/.test(number)
 
 function MasterHouseWorker(config) {
@@ -206,7 +211,7 @@ function TaskSystem(jobsArray = [], taskNumber = 5, setting = {}) {
   if (!Array.isArray(jobsArray)) console.log('jobsArray 僅接受 Array, 將使用空陣列')
   else this.jobsArray = [...jobsArray]
 
-  // 總共要起幾個task 去執行佇列, 預設值是 5
+  // 總共要起幾個 task 去執行佇列, 預設值是 5
   this.taskNumber = 5
   if (!isPositiveInt(taskNumber)) console.log('taskNumber 僅可為正整數, 將使用 5')
   else this.taskNumber = taskNumber
@@ -227,20 +232,36 @@ function TaskSystem(jobsArray = [], taskNumber = 5, setting = {}) {
   this.workingTasksNumber = 0 // 當前還沒結束的task 數量
   this.totalJobsNumber = this.jobsArray.length // 總任務數量
   this.finishedJobs = 0 // 完成的任務數量
+  this.progressbar = new cliProgress.SingleBar({
+    format: 'Progress {percentage}% {bar} {value}/{total}',
+    barCompleteChar: '\u2588',
+    barIncompleteChar: '\u2591',
+    // hideCursor: true,
+  }) // 進度條
 
   this.doPromise = () => {
+    // TODO 這裡會有互相遮蓋的問題，看要怎麼處理會比較好看
+    console.log('')
+    // process.stdout.cursorTo(0)
+    // process.stdout.clearLine()
+
     return new Promise((resolveOfDoPromise) => {
       if (this.jobsArray.length === 0) {
-        console.log('warning: 傳入的jobs 陣列為空')
+        console.log('warning: 傳入的jobs 陣列為空') // TODO 這句好像也可以不用加
         resolveOfDoPromise(this.resultArray)
         return
       }
 
-      console.log(`要執行的任務共有 ${this.jobsArray.length} 個`)
-      console.log(`分給 ${this.taskNumber} 個task 去執行`)
-      console.log(`每個task 約負責 ${Math.ceil(this.jobsArray.length / this.taskNumber)} 項任務`)
+      console.log(`Total jobs count: ${this.jobsArray.length}`)
+      console.log(`Task count: ${this.taskNumber}`)
+      console.log(
+        `Each task takes about ${Math.ceil(this.jobsArray.length / this.taskNumber)} jobs.`
+      )
 
       this.workingTasksNumber = this.taskNumber
+
+      this.progressbar.start(this.totalJobsNumber)
+
       for (var i = 0; i < this.taskNumber; i++) {
         _doJobs.call(this, resolveOfDoPromise)
       }
@@ -256,6 +277,7 @@ async function _doJobs(resolveOfDoPromise) {
     // 檢查現在還有沒有沒停止的task
     if (this.workingTasksNumber === 0) {
       console.log('') // 換行用
+      this.progressbar.stop()
       this.callback(this.resultArray)
 
       // doPromise 的resolve
@@ -283,11 +305,15 @@ async function _doJobs(resolveOfDoPromise) {
     console.log('job 失敗! 將重新嘗試')
     this.totalJobsNumber++
     this.jobsArray.push(job)
+
+    // TODO 加一筆總數
+    this.progressbar.setTotal(this.totalJobsNumber)
   }
 
   this.finishedJobs++
+  this.progressbar.increment()
 
-  _showOutput.call(this, jobReault)
+  // _showOutput.call(this, jobReault)
 
   this.eachCallback(jobReault)
   this.resultArray.push(jobReault)
@@ -334,20 +360,6 @@ function _checkParams(defaultSetting) {
       this.setting[settingKey]
     )
   })
-}
-
-function _showOutput(jobReault) {
-  // 秀給console 的文字
-  const status = jobReault.status ? 'success' : 'failed'
-  const of = `${this.finishedJobs} / ${this.totalJobsNumber}`
-  let persent = (parseInt(this.finishedJobs, 10) * 100) / parseInt(this.totalJobsNumber, 10)
-
-  persent = Math.round(persent * Math.pow(10, 2)) / 100
-  persent = `${persent.toFixed(2)}%`
-
-  process.stdout.clearLine()
-  process.stdout.cursorTo(0)
-  process.stdout.write(`${of}, ${persent}, ${status}`)
 }
 
 module.exports = TaskSystem
